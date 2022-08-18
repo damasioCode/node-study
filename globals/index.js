51650dd877285de51ea3cb45d0a56790c7ea746f
Daniel Damasio <damasio.dan7@gmail.com>
2022-08-17 22:10:17 -0300

    first commit

---
+++ globals/index.js
@@ -0,0 +1,12 @@
+// GLOBALS  - NO WINDOW !!!!
+
+// __dirname  - path to current directory
+// __filename - file name directory
+// require    - function to use modules (CommonJS)
+// module     - info about current module (file)
+// process    - info about env where the program is being executed
+
+setTimeout(() => {
+  console.log(__dirname)
+  console.log(__filename)
+},1000)
---
+++ intro/index.js
@@ -0,0 +1,7 @@
+const number = 10
+
+if ( number < 10 ) {
+  console.log('small number');
+} else {
+  console.log('big number');
+}
